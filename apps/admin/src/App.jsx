512dd62ec1ecd979db5ea102a13d3f7cbd22139a
Sabin Marcu <sabinmarcu@gmail.com>
2019-07-26 20:23:44 +0300

    Update App.jsx

--- apps/admin/src/App.jsx
+++ apps/admin/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 
-import Navigation from '@ct/layout';
+import { Navigation } from '@ct/layout';
 
 function App() {
   return (
