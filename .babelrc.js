f27ed5372791abbea09aa3d3e0aaf049a980a7cd
Sabin Marcu <sabin.marcu@rga.com>
2019-08-06 13:58:04 +0300

    fix: add babel graphql loader

--- .babelrc.js
+++ .babelrc.js
@@ -2,5 +2,10 @@ module.exports = {
   "presets": [
     "@babel/env",
     "@babel/flow"
+  ],
+  "plugins": [
+    "@babel/plugin-proposal-export-namespace-from",
+    "@babel/plugin-syntax-export-namespace-from",
+    ["import-graphql", { runtime: true }]
   ]
 };
